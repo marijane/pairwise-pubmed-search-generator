3e6ba686ec097b02349b8b5f6104f54c30fee55c
marijane white <marijane@users.noreply.github.com>
2024-10-25 04:40:34 +0000

    add noexp logic, fix keyword string generation

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,24 +3,28 @@ import streamlit as st
 pubmed_search_url = "https://pubmed.ncbi.nlm.nih.gov/?term="
 text_area_height = 250
 
-st.set_page_config(page_title="Pairwise PubMed Search Generator", layout="wide", page_icon="🔎")
-st.title("Pairwise PubMed Search Generator")
+st.set_page_config(page_title="Pairwise PubMed Search Generator", page_icon="🔎")
+st.title("Pairwise PubMed Search Generator", anchor=False)
 st.write("This app generates PubMed search strings from two lists of terms. Use it to combine a list of MeSH Main Headings with a list of MeSH Subheadings, or to work around the truncation linitation in PubMed's proximity search.")
 
 with st.form("enter_terms_form"):
-    st.header("Pairwise Mesh Main Heading/Subheading Search", divider=True)
+    st.header("Pairwise Mesh Main/Subheading Search", divider=True, anchor=False)
     mcol1, mcol2 = st.columns(2)
     with mcol1:
         mesh_terms = st.text_area("Enter MeSH Main Headings, one per line.", 
                                 height=text_area_height ,
                                 value="asthenia\nfatigue\nfrailty\nmuscle weakness\nmuscle atrophy").splitlines()
+        subcol1, subcol2 = st.columns(2)
+        with subcol1:
+            majr = st.checkbox("MeSH Major Topic")
+        with subcol2:
+            noexp = st.checkbox("Do not explode")
+
     with mcol2:
         subheadings = st.text_area("Enter MeSH Subheadings, one per line.", 
                                 height=text_area_height, value="diagnosis\nepidemiology").splitlines()
     
-    majr = st.checkbox("MeSH Major Topic")
-    
-    st.header("Pairwise Keyword Proximity Search", divider=True)
+    st.header("Pairwise Keyword Proximity Search", divider=True, anchor=False)
     pcol1, pcol2 = st.columns(2)
     with pcol1:
         proximity_topic1_terms = st.text_area("Enter Topic 1 terms, one per line, no truncation.", 
@@ -32,10 +36,10 @@ with st.form("enter_terms_form"):
         proximity_topic2_terms = st.text_area("Enter Topic 2 terms, one per line, no truncation.", 
                                     value="assess\nassessment\ndiagnosis\ndiagnoses\ndiagnostic\nevaluate\nevaluation\ninstrument\ninstruments\nindex\nmeasure\nmeasures\nscreen\nscreens\nscreening\nscreenings\ntest\ntests\ntesting\ntool\ntools",
                                     height=text_area_height, 
-                                    ).splitlines() 
+                                    ).splitlines()
         proximity_distance = st.text_input("Proximity distance", value="2")
 
-    st.header("Pairwise Keyword Intersection Search (Boolean AND)", divider=True)
+    st.header("Pairwise Keyword Intersection Search (Boolean AND)", divider=True, anchor=False)
     icol1, icol2 = st.columns(2)
     with icol1:
         intersection_topic1_terms = st.text_area("Enter Topic 1 terms, one per line", 
@@ -48,16 +52,21 @@ with st.form("enter_terms_form"):
                                     value="assess*\ndiagnos*\nevaluat*\ninstrument*\nindex\nindices\nmeasure*\nscreen*\ntest*\ntool*",
                                     height=text_area_height
                                     ).splitlines() 
+    
     submitted = st.form_submit_button("Generate pairwise search strings")
 
     if submitted:
+        st.subheader("Pairwise MeSH Main/Subheading Search String", divider=True, anchor=False)
         if majr:
-            field = "[majr]"
+            field = "majr"
         else:
-            field = "[mh]"
+            field = "mh"
+
+        if noexp:
+            field = field + ":noexp"
 
         mesh_searches = [
-            mesh_term + "/" + subheading + field
+            mesh_term + "/" + subheading + "[" + field + "]"
             for mesh_term in mesh_terms
             for subheading in subheadings
         ]
@@ -66,10 +75,11 @@ with st.form("enter_terms_form"):
         st.link_button(label="Search PubMed with pairwise MeSH heading/subheading search string",
                     url=pubmed_search_url+mesh_search_string.replace(" ", "+"))
 
+        st.subheader("Pairwise Keyword Proximity Search String", divider=True, anchor=False)
         keyword_proximity_searches = [
-            f'"{proximity_topic1_term} {proximity_topic2_term}"[{proximity_field}:~{proximity_distance}]'
-            for topic1_term in proximity_topic1_terms
-            for topic2_term in proximity_topic2_terms
+            f'"{ptopic1_term} {ptopic2_term}"[{proximity_field}:~{proximity_distance}]'
+            for ptopic1_term in proximity_topic1_terms
+            for ptopic2_term in proximity_topic2_terms
         ]
         keyword_proximity_search_string = " OR ".join(keyword_proximity_searches)
         st.write(keyword_proximity_search_string)
@@ -80,10 +90,11 @@ with st.form("enter_terms_form"):
         st.link_button(label="Search PubMed with pairwise MeSH/proximity search string",
                     url=pubmed_search_url+mesh_proximity_search_string.replace(" ", "+"))
 
+        st.subheader("Pairwise Keyword Intersection Search String", divider=True, anchor=False)
         keyword_intersection_searches = [
-            "(" + intersection_topic1_term + " AND " + intersection_topic2_term + ")"
-            for topic1_term in intersection_topic1_terms
-            for topic2_term in intersection_topic2_terms
+            "(" + itopic1_term + " AND " + itopic2_term + ")"
+            for itopic1_term in intersection_topic1_terms
+            for itopic2_term in intersection_topic2_terms
         ]
 
         keyword_intersection_search_string = " OR ".join(keyword_intersection_searches)
